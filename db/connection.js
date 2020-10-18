11e20f21e3dc96c2ff1222846e3a6b5b59f98822
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-18 19:31:37 -0400

    more progress on backend

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/library", { useNewUrlParser: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
-module.exports = mongoose;
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("connection with database established");
+});
+
+// mongoose.connect("mongodb://localhost/library", { useNewUrlParser: true });
+
+// module.exports = mongoose;
+
+//mongodb+srv://WRNoble:Carthage1@@cluster0.gtay8.mongodb.net/Library?retryWrites=true&w=majority
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello handsome");
 });
 
-app.listen(4000, () => {
-  console.log("app listening on port 4000");
+app.listen(port, () => {
+  console.log("app listening on port 5000");
 });
